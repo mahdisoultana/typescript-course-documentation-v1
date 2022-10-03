e4e46f002430e57c8acc041a7a59c2a5d6654203
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2022-10-03 20:19:42 +0100

    completing todo list

--- miniproject/dist/index.js
+++ miniproject/dist/index.js
@@ -1,8 +1,47 @@
 "use strict";
-const btn = document.getElementById("btn");
-// add event listener
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", (e) => {
-    console.log(e.target);
+let btn = document.getElementById("btn");
+const input = document.getElementById("todoInput");
+const list = document.querySelector("ul");
+//we make ! no nullish type assertion operator to tell typescript element is no null it's HTMLElement
+const form = document.querySelector("form");
+const todos = readTodos();
+todos.forEach((todo) => {
+    createTodo(todo);
 });
-let statusR = "mg";
-statusR = "hello";
+function readTodos() {
+    const todosString = localStorage.getItem("todos");
+    if (todosString == null)
+        return [];
+    const todos = JSON.parse(todosString);
+    return todos;
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    const todo = { text: input.value, completed: false };
+    todos.push(todo);
+    setStorageTodo();
+    createTodo(todo);
+}
+function setStorageTodo() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+function createTodo(todo) {
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("click", () => {
+        todo.completed = checkbox.checked;
+        setStorageTodo();
+    });
+    li.append(todo.text);
+    li.append(checkbox);
+    list.append(li);
+    input.value = "";
+}
+form.addEventListener("submit", handleSubmit);
+// add event listener
+// btn?.addEventListener("click", () => {
+//   alert("cliked");
+//   input.value = "";
+// });
--- miniproject/src/index.ts
+++ miniproject/src/index.ts
@@ -1,8 +1,66 @@
-const btn = document.getElementById("btn");
-// add event listener
-btn?.addEventListener("click", (e) => {
-  console.log(e.target);
-});
+let btn = document.getElementById("btn");
+const input = document.getElementById("todoInput")! as HTMLInputElement;
+const list = document.querySelector("ul")!;
 
-let statusR: string = "mg";
-statusR = "hello";
+//we make ! no nullish type assertion operator to tell typescript element is no null it's HTMLElement
+const form = document.querySelector("form")!;
+
+//here we have e=> event is known with the context of because its at a callback function
+// form.addEventListener("submit", (e) => {
+//   console.log("subimited");
+//   e.preventDefault();
+// });
+
+//but we if we have this method
+
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = readTodos();
+todos.forEach((todo) => {
+  createTodo(todo);
+});
+function readTodos(): Todo[] {
+  const todosString = localStorage.getItem("todos");
+  if (todosString == null) return [];
+  const todos = JSON.parse(todosString);
+  return todos;
+}
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+
+  const todo: Todo = { text: input.value, completed: false };
+  todos.push(todo);
+  setStorageTodo();
+  createTodo(todo);
+}
+
+function setStorageTodo() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+function createTodo(todo: Todo): void {
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
+  checkbox.addEventListener("click", () => {
+    todo.completed = checkbox.checked;
+    setStorageTodo();
+  });
+  li.append(todo.text);
+  li.append(checkbox);
+  list.append(li);
+  input.value = "";
+}
+
+form.addEventListener("submit", handleSubmit);
+
+// add event listener
+
+// btn?.addEventListener("click", () => {
+//   alert("cliked");
+//   input.value = "";
+// });

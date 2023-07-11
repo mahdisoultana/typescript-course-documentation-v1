2d62ef95d1e113108878cbf165c004a9197e7c8c
mahdisoultana <soultana.mahdi24@gmail.com>
2023-07-11 20:07:47 +0100

    space

--- typescript-book-documentation/src/index.ts
+++ typescript-book-documentation/src/index.ts
@@ -1,30 +1,31 @@
 function sayName(greating: string = ''): void {
-    const name: string = "mahdi";
-    console.log(`${greating} my Name is ${name}`)
+  const name: string = 'mahdi';
+  console.log(`${greating} my Name is ${name}`);
 }
 
 // sayName(34)
-sayName('Hi')
+sayName('Hi');
 // there is 5 syntaxes to write functions and typescript support that also
 
 //named function , i heared someone say function declaration
 
 function great(pram: string): void {
-    console.log(pram)
+  console.log(pram);
 }
 //function experssion
 const sayGreat = function (): void {
-    console.log('greating')
-}
+  console.log('greating');
+};
 
 // arrow function
-type Greating = (pram: string) => void
+type Greating = (pram: string) => void;
 const greating: Greating = (pram) => {
-    console.log(pram)
-}
+  console.log(pram);
+};
 //short hand arrow function
 
-const shortGreating: Greating = pram => console.log(pram)
+const shortGreating: Greating = (pram) => console.log(pram);
 
 //Function Constructor
+
 const greater = new Function('name', 'return "Greater" + name');
